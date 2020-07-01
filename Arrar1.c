d7af0ab559a8b4ec6763fb8287828c387259ff25
LwandaMagere <dadolenssen@gmail.com>
2020-07-01 07:41:47 +0300

    array

--- Arrar1.c
+++ Arrar1.c
@@ -21,26 +21,12 @@ int main()
         }
     if (max > 0) // there was a line
         printf("%s", longest);
-         
+
     return 0;
 }
 
 // read a line into s, return length
-int getline(char s[], int lim)
-{
-    int c, i;
 
-    for (i = 0; i < lim-1 && (c = getchar()) != 0 && c != '\n'; ++i)
-        s[i] = c;
-    if (c == '\n')
-    {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
-
-}
 
 // copy: copy 'from' into 'to'; assume to is big enough
 void copy(char to[], char from[])
@@ -51,3 +37,17 @@ void copy(char to[], char from[])
     while ((to[i] = from[i]) != '\0')
         ++i;
 }
+
+int getline(char s[],int lim)
+{
+int c, i;
+for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+s[i] = c;
+if (c == '\n') {
+s[i] = c;
+++i;
+}
+s[i] = '\0';
+return i;
+}
+
