191bea6847b0bc0cec5e6288f93384a6b36df434
LwandaMagere <dadolenssen@gmail.com>
2020-08-19 20:50:20 +0300

    returning a pointer

---
+++ returningptr.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int * findMid(int *, int );
+
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    int n = sizeof(a) / sizeof(a[0]);
+    int * mid = findMid(a, n);
+    printf("%d\n", *mid);
+    return 0;
+}
+
+int * findMid(int a[], int n)
+{
+    return &a[n/2];
+}
