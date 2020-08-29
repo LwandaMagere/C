a3cc8e55843f71c281929848d61eb74eccd94f88
LwandaMagere <dadolenssen@gmail.com>
2020-08-29 10:00:11 +0300

    input

---
+++ input.c
@@ -0,0 +1,16 @@
+// when to use &
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+    float assets;
+    char pet[30];
+
+    printf("Enter your age, assests, and favorite pet.\n");
+    scanf("%d %f", &age, &assets);
+    scanf("%s", pet);
+    printf("%d $%.2f %s\n", age, assets, pet);
+    
+    return 0;
+}
