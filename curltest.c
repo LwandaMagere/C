d853ae012014bffbc891507cc8bef6cddcd73f05
LwandaMagere <dadolenssen@gmail.com>
2020-08-30 21:34:32 +0300

    downloader

---
+++ curltest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(int argc, char **argv)
+{
+    CURL *curl;
+    FILE *fp;
+
+    fp = fopen(argv[2], "wb");
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+    curl_easy_setopt(curl,CURLOPT_WRITEDATA, fp);
+    curl_easy_setopt(curl,CURLOPT_FAILONERROR, 1);
+
+    int result = curl_easy_perform(curl);
+
+    if (result == CURLE_OK)
+        printf("Download is successful");
+    else 
+        printf("ERROR %s\n", curl_easy_strerror(result));
+
+    fclose(fp);
+    curl_easy_cleanup(curl);
+    return 0;
+}
