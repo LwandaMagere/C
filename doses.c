5fbbbe3fc9a37cadd9e685c9af816cf96f15071a
LwandaMagere <dadolenssen@gmail.com>
2020-09-05 12:10:34 +0300

    pointers

---
+++ doses.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char first_name[20];
+    char last_name[20];
+
+    puts("Enter the first name: ");
+    scanf("%19s %19s", first_name, last_name);
+    printf("First: %s Last:%s\n", first_name, last_name);
+    return 0;
+}
