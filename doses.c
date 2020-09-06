68945418639bddd2f23198f5951ed7a51d9b458a
LwandaMagere <dadolenssen@gmail.com>
2020-09-06 08:16:53 +0300

    pointer

--- doses.c
+++ doses.c
@@ -5,6 +5,8 @@ int main()
     char first_name[20];
     char last_name[20];
 
+    char *cards="JQK";
+
     puts("Enter the first name: ");
     scanf("%19s %19s", first_name, last_name);
     printf("First: %s Last:%s\n", first_name, last_name);
