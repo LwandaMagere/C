70eca60195ca2682ba5af636fbf7d6298e794b67
LwandaMagere <dadolenssen@gmail.com>
2020-09-13 20:10:27 +0300

    header file

--- ifdef.c
+++ ifdef.c
@@ -10,6 +10,7 @@ int main()
 
     for (i = 1; i <= LIMIT; i++)
     {
+        total += 2*i*i + 1;
         #ifdef JUST_CHECKING
             printf("i=%d, running total = %d\n", i, total);
         #endif
