c351f32bbb9b1a0c946393550a387d155a9e9859
Steven Dado <dadolenssen@gmail.com>
2021-05-20 11:06:25 +0300

    d

--- Array.c
+++ Array.c
@@ -2,49 +2,7 @@
 /* count digits, white space, others */
 int main()
 {
-     puts("C Rocks Bana!\n\n");
-
-     int card_count = 11;
-     if (card_count > 10)
-        puts("The deck is hot. Increase bet.\n");
-
-    int decks;
-    puts("Enter a number of decks\n");
-    scanf("%i", &decks);
-    if (decks < 1)
-    {
-        puts("That is not a valid number of decks\n");
-        return 1;
-    }
-    printf("There are %i cards\n", (decks * 52));
-    
-    int c = 10;
-    while (c > 0)
-    {
-        puts("I must not write code in class\n");
-        c = c -1;
-    }
-
-    char ex[20];
-    puts("Enter boyfriend's name: \n");
-    scanf("%19s", ex);
-    printf("Dear %s.\n\n\tYou're history.\n", ex);
-
-    char suit = 'H';
-    switch(suit)
-    {
-        case 'C':
-            puts("Clubs");
-            break;
-        case 'D':
-            puts("Diamond");
-            break;
-        case 'H':
-            puts("Hearts");
-            break;
-        default:
-            puts("Spades");
-    }
+     
 
 
 
