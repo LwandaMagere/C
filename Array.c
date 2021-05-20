bfa05aab8038ac22adab700501d73a31f24a3995
Steven Dado <dadolenssen@gmail.com>
2021-05-20 12:18:19 +0300

    r

--- Array.c
+++ Array.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 /* count digits, white space, others */
 int main()
 {
-     
-
+    char card_name[3];
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    if (card_name[0] == 'K')
+    {
+        val = 10;
+    }
+    else if ( card_name[0] == 'Q')
+        val = 10;
+        
 
 
 
