4baa7e4e40a08bc382f3337d4591c1c04225eb70
Steven Dado <dadolenssen@gmail.com>
2021-06-24 12:00:26 +0300

    g

--- Array.c
+++ Array.c
@@ -6,37 +6,15 @@
 
 int main()
 {
-    int card_count =  11;
-    if (card_count > 10)
-        puts("The deck is hot. Increase bet.");
-
-    int c  = 10;
-    while (c > 0)
+    int decks;
+    puts("Enter a number of decks");
+    scanf("%i", &decks);
+    if (decks < 1)
     {
-        puts("I must not write code in class");
-        c = c - 1;
+        puts("That is not a valid number of decks");
+        return 1;
     }
-
-    char ex[20];
-    puts("Enter girlfriend's name: ");
-    scanf("%19s", ex);
-    printf("Dear %s.\n\n\tYou're history.\n", ex);
-
-    char suit = 'H';
-    switch(suit)
-    {
-        case 'C':
-            puts("Clubs");
-            break;
-        case 'D':
-            puts("Diamond");
-            break;
-        case 'H':
-            puts("Hearts");
-        default:
-            puts("Spades");
-    }
-    
+    printf("There are %i cards\n", (decks * 52));
 
 
    /* int c, i, nwhite, nother;
