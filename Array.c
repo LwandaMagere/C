f84b0e12b03ac0e1fcc092a68aaea64a7dceff19
Steven Dado <dadolenssen@gmail.com>
2021-07-02 14:44:30 +0300

    r

--- Array.c
+++ Array.c
@@ -7,32 +7,35 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    if (card_name[0] == 'K')
+    int count = 0;
+    while (card_name[0] != 'X')
     {
-        val  = 10;
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0])
+        {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10))
+                {
+                    puts("I don't understand that value");
+                    continue;
+                }
+        }
     }
-    else if (card_name[0] == 'Q')
-    {
-        val = 10;
-    }
-    else if (card_name[0] == 'J')
-    {
-        val = 10;
-    }
-    else if (card_name[0] == 'A')
-    {
-        val = 11;
-    }
-    else 
-    {
-        val = atoi(card_name);
-    }
-
-    printf("The card value is: %i\n", val);
-
+        
+   
    
 
    /* int c, i, nwhite, nother;
