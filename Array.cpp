ae2e7e5a26a6eeac471cfd50f8d9a8dec8bb6fa3
Steven Dado <dadolenssen@gmail.com>
2021-07-08 10:30:29 +0300

    r

--- Array.cpp
+++ Array.cpp
@@ -1,20 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iostream>
-
-using namespace std;
-
-
 
  
 
 int main()
 {
-    
-    
+    char card_name[3];
+    int count = 0;
+    while (card_name[0] != 'X')
+    {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0])
+        {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val > 10))
+                {
+                    puts("I don't understand that value");
+                    continue;
+                }
+        }
+    }
         
    
+   
 
+   /* int c, i, nwhite, nother;
+    int ndigit[10];
+
+    nwhite = nother = 0;
+    for (i = 0; i < 10; ++i)
+        ndigit[i] = 0;
+
+    while ((c = getchar()) != '\n')
+        if (c >= '0' && c <= '9')
+            ++ndigit[c - '0'];
+        else if (c == ' ' || c == '\n' || c == '\t')  
+            ++nwhite;
+        else 
+            ++nother;
+    printf("digits = ");
+    for (i = 0; i < 10; ++i)
+        printf(" %d", ndigit[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
+    printf("%d", ndigit[c - '0']);*/
     return 0;
 }
