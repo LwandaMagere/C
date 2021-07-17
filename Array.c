5917455b0e62427063eab1808f44c8d3dd84d40a
Steven Dado <dadolenssen@gmail.com>
2021-07-18 00:28:46 +0300

    m

--- Array.c
+++ Array.c
@@ -1,49 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define JUST_CHECKING
+#define LIMIT 4
 
-int main()
+
+int main(void)
 {
-    
-    char line[80];
-    FILE *in = fopen("sppoky.csv", "r");
-    FILE *file1 = fopen("ufos.csv", "w");
-    FILE *file2 = fopen("disappearance.csv", "w");
-    FILE *file3 = fopen("others.csv", "w");
-    while (fscanf(in, "%79[^\n]", line) == 1)
+    int i;
+    int total  = 0;
+
+    for (i = 1; i <= LIMIT; i++)
     {
-        if (strstr(line, "UFO"))
-            fprintf(file1, "%s\n", line);
-        else if (strstr(line, "Disappearance"))
-            fprintf(file2, "%s\n", line);
-        else 
-            fprintf(file3, "%s\n", line);
+        total += 2*i*i + 1;
+#ifdef JUST_CHECKING
+        printf("i=%d, running total = %d\n", i, total);
+#endif
     }
-    fclose(file1);
-    fclose(file2);
-    fclose(file3);
+    printf("Grand total = %d\n", total);
 
-
-
-
-   /* int c, i, nwhite, nother;
-    int ndigit[10];
-
-    nwhite = nother = 0;
-    for (i = 0; i < 10; ++i)
-        ndigit[i] = 0;
-
-    while ((c = getchar()) != '\n')
-        if (c >= '0' && c <= '9')
-            ++ndigit[c - '0'];
-        else if (c == ' ' || c == '\n' || c == '\t')  
-            ++nwhite;
-        else 
-            ++nother;
-    printf("digits = ");
-    for (i = 0; i < 10; ++i)
-        printf(" %d", ndigit[i]);
-    printf(", white space = %d, other = %d\n", nwhite, nother);
-    printf("%d", ndigit[c - '0']);*/
+    
     return 0;
 }
