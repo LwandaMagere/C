5bff7ca3434af7aeb1d1a810a9f1d93e6941f820
Steven Dado <dadolenssen@gmail.com>
2021-08-18 10:16:23 +0300

    r

--- Array.c
+++ Array.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 #define DENSITY 62.4
 
 int main()
 {
-    float weight, volume;
-    int size, letters;
-    char name[40];
-
-    printf("Hi! What's your first name?\n");
-    scanf("%s", name);
-    printf("%s, what's your weight in pounds?\n", name);
-    scanf("%f", &weight);
-    size = sizeof name;
-    letters = strlen(name);
-    printf("Well, %s, your volume is %2.2f cubic feet.\n", name, volume);
-    printf("Also, your first name has %d letters,\n", letters);
-    printf("and we have %d bytes to store it.\n", size);
+    int32_t me32;
+    
+    me32 = 345654654;
+    printf("me32 = %" PRId32 "\n", me32);
+    
     return 0;
 }
